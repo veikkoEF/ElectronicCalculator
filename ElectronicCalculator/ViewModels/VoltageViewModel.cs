65a31929fb9b27bd715a278fbca6978369239ae8
veikko2000@yahoo.de <Veikko>
2023-03-05 15:51:14 +0100

    Views Volatge, Info,

--- ElectronicCalculator/ViewModels/VoltageViewModel.cs
+++ ElectronicCalculator/ViewModels/VoltageViewModel.cs
@@ -2,4 +2,30 @@
 
 public partial class VoltageViewModel : BaseViewModel
 {
-}
+    [ObservableProperty]
+    private double voltage;
+
+    [ObservableProperty]
+    private double resistor1 = 240;
+
+    [ObservableProperty]
+    private double resistor2;
+
+    [ObservableProperty]
+    private bool voltageMode = true;
+
+    [RelayCommand]
+    private void Calculate()
+    {
+        if (voltageMode)
+        {
+            Voltage = 1.25 * (1 + Resistor2 / Resistor1);
+        }
+        else
+        {
+            Resistor2 = Resistor1 * (Voltage / 1.25 - 1);
+        }
+    }
+
+
+    }
--- ElectronicCalculator/Views/VoltagePage.xaml.cs
+++ ElectronicCalculator/Views/VoltagePage.xaml.cs
@@ -2,9 +2,28 @@
 
 public partial class VoltagePage : ContentPage
 {
-	public VoltagePage(VoltageViewModel viewModel)
+	private VoltageViewModel _viewModel;
+    public VoltagePage(VoltageViewModel viewModel)
 	{
 		InitializeComponent();
+		_viewModel= viewModel;
 		BindingContext = viewModel;
+
 	}
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+		if (RadioButtonR2.IsChecked)
+		{
+			EntryR2.IsReadOnly = true;
+			EntryVoltage.IsReadOnly = false;
+            _viewModel.VoltageMode = false;
+        }
+		else
+		{
+			EntryR2.IsReadOnly = false;
+            EntryVoltage.IsReadOnly = true;
+			_viewModel.VoltageMode = true;
+        }
+    }
 }
